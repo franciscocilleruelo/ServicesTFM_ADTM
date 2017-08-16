e624ea87a82a2e2ed615862f58091edb4c414b1b
FRANCISCO <FRANCISCO@FRAN>
2017-08-16 19:11:03 +0200

    First commit

---
+++ CustomerService_ADTM/src/main/java/es/uned/master/software/tfm/adtm/service/customer/CustomerServiceAdtmApplication.java
@@ -0,0 +1,12 @@
+package es.uned.master.software.tfm.adtm.service.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceAdtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceAdtmApplication.class, args);
+	}
+}
