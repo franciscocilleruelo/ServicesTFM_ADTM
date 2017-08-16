026240d82c9bceaeebaf5ed5e5e0f88849fadea2
FRANCISCO <FRANCISCO@FRAN>
2017-08-16 19:15:29 +0200

    Commit Order Service

---
+++ OrderService_ADTM/src/test/java/es/uned/master/software/tfm/adtm/service/order/OrderServiceAdtmApplicationTests.java
@@ -0,0 +1,16 @@
+package es.uned.master.software.tfm.adtm.service.order;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderServiceAdtmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
