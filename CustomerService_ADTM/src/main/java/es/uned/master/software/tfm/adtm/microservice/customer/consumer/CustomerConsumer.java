aabd1d4fc7414ff57a2eb02f87eb9870a3a71a75
Francisco Cilleruelo <Francisco Cilleruelo@FRAN>
2017-09-01 19:55:01 +0200

    Modificacion en la integracion ADTM con microservicios

--- CustomerService_ADTM/src/main/java/es/uned/master/software/tfm/adtm/microservice/customer/consumer/CustomerConsumer.java
+++ CustomerService_ADTM/src/main/java/es/uned/master/software/tfm/adtm/microservice/customer/consumer/CustomerConsumer.java
@@ -6,15 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import es.uned.master.software.tfm.adtm.amqp.receiver.ReceiverConsumer;
-import es.uned.master.software.tfm.adtm.microservice.customer.jpa.entity.Customer;
 import es.uned.master.software.tfm.adtm.microservice.customer.jpa.entity.Order;
-import es.uned.master.software.tfm.adtm.microservice.customer.jpa.entity.ReservedCredit;
-import es.uned.master.software.tfm.adtm.microservice.customer.jpa.entity.ReservedCreditId;
-import es.uned.master.software.tfm.adtm.microservice.customer.jpa.repository.CustomerRepository;
-import es.uned.master.software.tfm.adtm.microservice.customer.jpa.repository.ReservedCreditRepository;
+import es.uned.master.software.tfm.adtm.microservice.customer.service.CustomerService;
 
 @Component
 public class CustomerConsumer extends ReceiverConsumer<Order> implements Serializable{
@@ -24,10 +19,7 @@ public class CustomerConsumer extends ReceiverConsumer<Order> implements Seriali
 	private static final Logger log = LoggerFactory.getLogger(CustomerConsumer.class);
 	
 	@Autowired
-	private CustomerRepository customerRepository;
-	
-	@Autowired
-	private ReservedCreditRepository reservedCreditRepository;
+	private CustomerService customerService;
 
 	public CustomerConsumer() {
 		super(Order.class);
@@ -35,28 +27,8 @@ public class CustomerConsumer extends ReceiverConsumer<Order> implements Seriali
 
 	@Override
 	public boolean processRequest(Order order) {
-		log.info("Recuperamos el cliente {} asociado al pedido", order.getCustomerId());
-		Customer customer = customerRepository.findOne(order.getCustomerId());
-		int reservedCreditNow = 0;
-		String reservedCreditNowS = reservedCreditRepository.sumReserverCreditByCustomerId(order.getCustomerId());
-		if (StringUtils.hasText(reservedCreditNowS)){
-			reservedCreditNow = Integer.valueOf(reservedCreditNowS);
-		}
-		log.info("El credito reservado del cliente {} para otros pedidos es de {}", order.getCustomerId(), reservedCreditNow);
-		if (customer != null && customer.getCreditLimit() >= order.getTotal() + reservedCreditNow){
-			log.info("El limite de credito para el cliente {} es superior a la suma de la cantidad solicitada para el pedido ({}) mas el credito reservado para otros pedidos ({})"
-					, order.getCustomerId(), order.getOrderId(), reservedCreditNow);
-			ReservedCreditId reservedCreditId = new ReservedCreditId(order.getOrderId(), order.getCustomerId());
-			ReservedCredit reservedCredit = new ReservedCredit(reservedCreditId, order.getTotal());
-			log.info("Se reserva el credito {} para el pedido {} del cliente {}", reservedCredit.getTotalReserved(), 
-					reservedCredit.getReservedCreditId().getOrderId(), reservedCredit.getReservedCreditId().getCustomerId());
-			reservedCreditRepository.save(reservedCredit);
-			return true;
-		} else { // No existe el cliente o la cantidad del pedido supera el credito
-			log.info("El limite de credito es inferior a la cantidad solicitada por el pedido");
-			return false;
-		}
-		
+		log.info("Comprobamos la disponibilidad de credito para el cliente {}", order.getCustomerId());
+		return customerService.checkCredit(order);
 	}
 
 }
--- CustomerService_ADTM/src/main/java/es/uned/master/software/tfm/adtm/microservice/customer/service/CustomerService.java
+++ CustomerService_ADTM/src/main/java/es/uned/master/software/tfm/adtm/microservice/customer/service/CustomerService.java
@@ -7,9 +7,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import es.uned.master.software.tfm.adtm.microservice.customer.jpa.entity.Customer;
+import es.uned.master.software.tfm.adtm.microservice.customer.jpa.entity.Order;
+import es.uned.master.software.tfm.adtm.microservice.customer.jpa.entity.ReservedCredit;
+import es.uned.master.software.tfm.adtm.microservice.customer.jpa.entity.ReservedCreditId;
 import es.uned.master.software.tfm.adtm.microservice.customer.jpa.repository.CustomerRepository;
+import es.uned.master.software.tfm.adtm.microservice.customer.jpa.repository.ReservedCreditRepository;
 
 
 @Service
@@ -21,6 +26,9 @@ public class CustomerService {
 	@Autowired
 	private CustomerRepository customerRepository;
 	
+	@Autowired
+	private ReservedCreditRepository reservedCreditRepository;
+	
 	public void insertExampleData(){
 		customerRepository.save(new Customer(300));
 		customerRepository.save(new Customer(8000));
@@ -30,6 +38,30 @@ public class CustomerService {
 	public List<Customer> findAll(){
 		log.info("Busqueda de todos los clientes");
 		return customerRepository.findAll();
+	}
+	
+	public boolean checkCredit(Order order){
+		log.info("Recuperamos el cliente {} asociado al pedido", order.getCustomerId());
+		Customer customer = customerRepository.findOne(order.getCustomerId());
+		int reservedCreditNow = 0;
+		String reservedCreditNowS = reservedCreditRepository.sumReserverCreditByCustomerId(order.getCustomerId());
+		if (StringUtils.hasText(reservedCreditNowS)){
+			reservedCreditNow = Integer.valueOf(reservedCreditNowS);
+		}
+		log.info("El credito reservado del cliente {} para otros pedidos es de {}", order.getCustomerId(), reservedCreditNow);
+		if (customer != null && customer.getCreditLimit() >= order.getTotal() + reservedCreditNow){
+			log.info("El limite de credito para el cliente {} es superior a la suma de la cantidad solicitada para el pedido ({}) mas el credito reservado para otros pedidos ({})"
+					, order.getCustomerId(), order.getOrderId(), reservedCreditNow);
+			ReservedCreditId reservedCreditId = new ReservedCreditId(order.getOrderId(), order.getCustomerId());
+			ReservedCredit reservedCredit = new ReservedCredit(reservedCreditId, order.getTotal());
+			log.info("Se reserva el credito {} para el pedido {} del cliente {}", reservedCredit.getTotalReserved(), 
+					reservedCredit.getReservedCreditId().getOrderId(), reservedCredit.getReservedCreditId().getCustomerId());
+			reservedCreditRepository.save(reservedCredit);
+			return true;
+		} else { // No existe el cliente o la cantidad del pedido supera el credito
+			log.info("El limite de credito es inferior a la cantidad solicitada por el pedido");
+			return false;
+		}
 	}
 
 }
